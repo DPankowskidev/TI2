9b2527d7b7711b198ef0a0c55b88c0345713d395
DPankowski <255395@edu.p.lodz.pl>
2025-05-05 18:06:05 +0200

    added function add

---
+++ cw_7_kalkulator/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+int main(int argc, char const *argv[]) {
+    if (argc == 4) {
+        int a = 0;
+        int b = 0;
+        a = atoi(argv[1]);
+        b = atoi(argv[3]);
+
+        if (strcmp(argv[2], "add") == 0) {
+            printf("%d + %d = %d\n", a, b, a + b);
+            return 0;
+        }
+    }
+}
